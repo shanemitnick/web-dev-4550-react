31bd165f8d081c260892051d5c317c81925ccf54
shanemitnick <shane.mitnick21@gmail.com>
2022-03-18 08:17:24 -0700

    using CDN for bootstrap css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Labs  from "./components/Labs/index";
 import Index from "./components/tuiter";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
-import './vendors/bootstrap.min.css';
+
 import './vendors/bootstrap.bundle.min';
 import './vendors/fontawesome-free-5.15.4-web/css/all.css';
 
